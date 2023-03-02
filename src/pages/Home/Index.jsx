2bb8d0d93ee054b0c9296ca59a8b00291b76115d
4212ganteng <azizmuslim051@gmail.com.com>
2023-03-02 11:53:10 +0700

    react-to-print

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,4 +1,6 @@
+import { useRef } from "react";
 import QRCode from "react-qr-code";
+import ReactToPrint from "react-to-print";
 
 const Home = () => {
   const datas = [
@@ -13,16 +15,30 @@ const Home = () => {
   const data = JSON.stringify(datas);
 
   console.log({ data });
+  // buat variables
+  const componentRef = useRef();
   return (
-    <div>
-      {datas.map((item) => (
-        <QRCode
-          value={`ts-product: ${item.alamat},${item.nama}`}
-          size={225}
-          style={{ margin: 25 }}
-        />
-      ))}
-    </div>
+    <>
+      {/* print */}
+      <ReactToPrint
+        trigger={() => (
+          <button
+            type="primary"
+            style={{ backgroundColor: "green", borderColor: "green" }}
+            shape="round"
+          >
+            Cetak Barcode
+          </button>
+        )}
+        // componen yang akan di print
+        content={() => componentRef.current}
+      />
+
+      {/* define komponen yang akan di print dengan memasukan var= ref={componentRef}*/}
+      <div ref={componentRef}>
+        <h1>hello world</h1>
+      </div>
+    </>
   );
 };
 
